aa29529562448ba87e8806909ec5076a63b0e7a8
Tik <tik65536@gmail.com>
2025-11-14 09:38:28 +0200

    #12 - updated dependencies for xdggs 0.4.0 - updated dggrid4py to latest release - separated IGEO7Info into `dependences/grids.py`, which aim to support other grids with dggrid - introduced `GridsConfig` dictionary to hold grids meta config - new attributes for IGEO7Info:   - _wgs84_geodetic_conversion   - _dggs_vert0_lon - rename IGEO7.py to index.py. - when instantiate IGEO7Index, it use the corresponding   grid config from `GridConfig` as a template and update the meta data   if need to create the `IGEO7Info` object.

---
+++ xdggs_dggrid4py/dependences/grids.py
@@ -0,0 +1,115 @@
+from xdggs.grid import DGGSInfo
+from typing import Any, ClassVar, Self
+from dataclasses import dataclass
+import numpy as np
+import os
+import decimal
+import tempfile
+import shapely
+
+from dggrid4py import DGGRIDv8
+from dggrid4py.auxlat import geoseries_to_authalic, geoseries_to_geodetic
+from geopandas.geoseries import GeoSeries
+
+
+try:
+    dggrid_path = os.environ['DGGRID_PATH']
+except KeyError:
+    raise Exception("DGGRID_PATH env var not found")
+
+GridsConfig = {'IGEO7': {"refinement_level_range": range(0, 22),
+                         "metaconfig": {"input_address_type":  'HIERNDX',
+                                        "input_hier_ndx_system": 'Z7',
+                                        "input_hier_ndx_form":  'int64',  # defaults to int representation
+                                        "output_address_type":  'HIERNDX',
+                                        "output_cell_label_type":  'OUTPUT_ADDRESS_TYPE',
+                                        "output_hier_ndx_system":  'Z7',
+                                        "output_hier_ndx_form":  'int64',  # defaults to int representation
+                                        "dggs_vert0_lon": 11.20
+                                        }
+                         },
+               'ISEA7H': {"refinement_level_range": range(0, 22),
+                          "metaconfig": {"input_address_type":  'HIERNDX',
+                                         "input_hier_ndx_system": 'SEQNUM',
+                                         "input_hier_ndx_form":  'int64',  # defaults to int representation
+                                         "output_address_type":  'HIERNDX',
+                                         "output_cell_label_type":  'OUTPUT_ADDRESS_TYPE',
+                                         "output_hier_ndx_system":  'SEQNUM',
+                                         "output_hier_ndx_form":  'int64',  # defaults to int representation
+                                         "dggs_vert0_lon": 11.20
+                                         }
+                          },
+               }
+
+
+# Alway returns a GeoSeries
+def _authalic_to_geodetic(geometry, convert: bool) -> GeoSeries:
+    if (not isinstance(geometry, GeoSeries)):
+        geometry = GeoSeries(geometry)
+    if (not convert):
+        return geometry
+    return geoseries_to_geodetic(geometry)
+
+
+# Alway returns a GeoSeries
+def _geodetic_to_authalic(geometry, convert: bool) -> GeoSeries:
+    if (not isinstance(geometry, GeoSeries)):
+        geometry = GeoSeries(geometry)
+    if (not convert):
+        return geometry
+    return geoseries_to_authalic(geometry)
+
+
+@dataclass(frozen=True)
+class IGEO7Info(DGGSInfo):
+    grid_name: str
+    _dggrid_meta_config: dict
+    _wgs84_geodetic_conversion: bool = True
+    _dggs_vert0_lon: decimal.Decimal | float = 11.20
+    _dggrid = DGGRIDv8(dggrid_path, tempfile.TemporaryDirectory().name, silent=True)
+
+    valid_parameters: ClassVar[dict[str, Any]] = {"level": GridsConfig["IGEO7"]["refinement_level_range"]}
+
+    def __post_init__(self):
+        if (self.grid_name.upper() != 'IGEO7'):
+            raise ValueError('Wrong grid_name for IGEO7Info.')
+        if self.level not in self.valid_parameters["level"]:
+            raise ValueError(f'level must be an integer between {GridsConfig["IGEO7"]["refinement_level_range"]}.')
+
+    @classmethod
+    def from_dict(cls: type[Self], mapping: dict[str, Any]) -> Self:
+        params = {k: v for k, v in mapping.items()}
+        return cls(**params)
+
+    def to_dict(self: Self) -> dict[str, Any]:
+        return {"level": self.level, "grid_name": self.grid_name,
+                "_wgs84_geodetic_conversion": self._wgs84_geodetic_conversion,
+                "_dggs_vert0_lon": self._dggs_vert0_lon}
+
+    def cell_ids2geographic(
+        self, cell_ids: np.ndarray
+    ) -> tuple[np.ndarray, np.ndarray]:
+        centroids_df = self._dggrid.grid_cell_centroids_from_cellids(cell_ids, self.grid_name, self.level,
+                                                                     **metaconfig)
+        centroids_df.geometry = _authalic_to_geodetic(centroids_df.geometry, self._wgs84_geodetic_conversion)
+        centroids = centroids_df.get_coordinates()
+        return (centroids['x'].values, centroids['y'].values)
+
+    def geographic2cell_ids(self, lon, lat):
+        assert len(lon) == len(lat), f"{__name__} the length of lon and lat are not equal"
+        centroids = GeoSeries([shapely.Point(c[0], c[1]) for c in zip(lon, lat)])
+        centroids = _geodetic_to_authalic(centroids, self._wgs84_geodetic_conversion)
+        centroids = self._dggrid.cells_for_geo_points(centroids, True, self.grid_name, self.level, **metaconfig)
+        return centroids['name'].values
+
+    def cell_boundaries(self, cell_ids, backend="shapely"):
+        if (backend != "shapely"):
+            raise NotImplementedError("Only shapely is implemeneted")
+        hexagon_df = self._dggrid.grid_cell_polygons_from_cellids(cell_ids, self.grid_name,
+                                                                  self.level,
+                                                                  **metaconfig)
+        geometry = _authalic_to_geodetic(hexagon_df.geometry, self._wgs84_geodetic_conversion)
+        return geometry.values
+
+    def zoom_to(self, cell_ids, level: int):
+        raise NotImplementedError("")
---
+++ xdggs_dggrid4py/index.py
@@ -0,0 +1,55 @@
+from collections.abc import Mapping
+
+import xarray as xr
+from xarray.indexes import PandasIndex
+
+from xdggs.index import DGGSIndex
+from xdggs.grid import DGGSInfo
+from xdggs.utils import register_dggs, _extract_cell_id_variable, GRID_REGISTRY
+from typing import Any
+from xdggs_dggrid4py.dependences.grids import IGEO7Info, GridsConfig
+
+
+@register_dggs("igeo7")
+class IGEO7Index(DGGSIndex):
+    _grid: DGGSInfo
+
+    def __init__(
+        self,
+        cell_ids: Any | PandasIndex,
+        dim: str,
+        grid_info: DGGSInfo,
+    ):
+        if not isinstance(grid_info, DGGSInfo):
+            raise ValueError(f"grid info object has an invalid type: {type(grid_info)}")
+        super().__init__(cell_ids, dim, grid_info)
+
+    @classmethod
+    def from_variables(cls: type["IGEO7Index"], variables: Mapping[Any, xr.Variable],
+                       *, options: Mapping[str, Any],) -> "IGEO7Index":
+        _, var, dim = _extract_cell_id_variable(variables)
+        attrs = var.attrs.copy()
+        attrs["grid_name"] = attrs["grid_name"].upper()
+        cls = GRID_REGISTRY.get(attrs["grid_name"].lower())
+        if cls is None:
+            raise ValueError(f'unknown DGGS grid name: {var.attrs["grid_name"]}.')
+        if (attrs["grid_name"] != 'IGEO7'):
+            raise ValueError(f'The grid_name({attrs["grid_name"]}) is wrong for the IGEO7Index.')
+        metaconfig = GridsConfig["IGEO7"]["metaconfig"]
+        metaconfig.update({"dggs_vert0_lon": attrs.get("_dggs_vert0_lon", 11.20)})
+        if (isinstance(var.values[0], str)):
+            metaconfig.update({"input_hier_ndx_form": "DIGIT_STRING"})
+            metaconfig.update({"output_hier_ndx_form": "DIGIT_STRING"})
+        attrs.update({"_dggrid_meta_config": metaconfig})
+        igeo7info = IGEO7Info.from_dict(attrs)
+        return cls(var.data, dim, igeo7info)
+
+    @property
+    def grid_info(self) -> IGEO7Info:
+        return self._grid
+
+    def _repr_inline_(self, max_width: int):
+        return f"IGEO7Index(level={self._grid.level})"
+
+    def _replace(self, new_index: PandasIndex):
+        return type(self)(new_index, self._dim, self._grid)
