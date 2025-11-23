df5cb328aa1bfcf1372bfbc0853089a46baf73b3
Tik <tik65536@gmail.com>
2025-11-23 14:31:02 +0200

    #12 - renamed the module name `point_based` to `centroid_based` under the folder `methods`.

--- xdggs_dggrid4py/regridding/methods/__init__.py
+++
@@ -1,4 +0,0 @@
-from xdggs_dggrid4py.regridding.methods.point_based.centerpoint import centerpoint
-from xdggs_dggrid4py.regridding.methods.point_based.nearestpoint import nearestpoint, mapblocks_nearestpoint
-
-__all__ = ['centerpoint', 'nearestpoint', 'mapblocks_nearestpoint']
---
+++ xdggs_dggrid4py/regridding/methods/centroid_based/__init__.py
@@ -0,0 +1,4 @@
+from xdggs_dggrid4py.regridding.methods.centroid_based.centerpoint import centerpoint
+from xdggs_dggrid4py.regridding.methods.centroid_based.nearestpoint import nearestpoint, mapblocks_nearestpoint
+
+__all__ = ["centerpoint", "nearestpoint", "mapblocks_nearestpoint"]
